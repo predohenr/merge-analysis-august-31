fadc86127d5495505924dcd0e9bf5b2aec7600ce
Mining Worker <miningworker@gmail.com>
2026-09-02 16:37:43 +0000

    Mining Framework Analysis for merge.mergiraf_semi.py

--- spyder/plugins/editor/confpage.py
+++ spyder/plugins/editor/confpage.py
@@ -5,20 +5,19 @@
 # (see spyder/__init__.py for details)
 
 """Editor config page."""
-
-from itertools import combinations
-
+from spyder.config.manager import CONF
+from spyder.api.config.decorators import on_conf_change
+from qtpy.QtCore import Qt, Signal
+from spyder.config.base import _
 from qtpy.QtWidgets import (QGridLayout, QGroupBox, QHBoxLayout, QLabel,
                             QVBoxLayout, QDialog, QDialogButtonBox, QWidget,
                             QCheckBox, QSizePolicy)
-from qtpy.QtCore import Qt, Signal
-
-from spyder.api.config.decorators import on_conf_change
 from spyder.api.config.mixins import SpyderConfigurationObserver
 from spyder.api.preferences import PluginConfigPage
-from spyder.config.base import _
-from spyder.config.manager import CONF
+import sys
+import os
 from spyder.utils.icon_manager import ima
+from itertools import combinations
 from spyder.widgets.helperwidgets import TipWidget
 
 
