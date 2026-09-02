972208c6c321bd0414f84addd30c927b4c9299ed
Mining Worker <miningworker@gmail.com>
2026-09-02 17:00:09 +0000

    Mining Framework Analysis for merge.mergiraf_semi_plus.py

--- src/twisted/conch/ssh/keys.py
+++ src/twisted/conch/ssh/keys.py
@@ -147,6 +147,49 @@ def _normalizePassphrase(passphrase):
         return passphrase
 
 
+def _getPersistentRSAKey(location, keySize=4096):
+    """
+    This function returns a persistent L{Key}.
+
+    The key is loaded from a PEM file in C{location}. If it does not exist, a
+    key with the key size of C{keySize} is generated and saved.
+
+    @param location: Where the key is stored.
+    @type location: L{twisted.python.filepath.FilePath}
+
+    @param keySize: The size of the key, if it needs to be generated.
+    @type keySize: L{int}
+
+    @returns: A persistent key.
+    @rtype: L{Key}
+    """
+    location.parent().makedirs(ignoreExistingDirectory=True)
+
+    # If it doesn't exist, we want to generate a new key and save it
+    if not location.exists():
+        privateKey = rsa.generate_private_key(
+            public_exponent=65537, key_size=keySize, backend=default_backend()
+        )
+
+        pem = privateKey.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.TraditionalOpenSSL,
+            encryption_algorithm=serialization.NoEncryption(),
+        )
+
+        location.setContent(pem)
+
+    # By this point (save any hilarious race conditions) we should have a
+    # working PEM file. Load it!
+    # (Future archaeological readers: I chose not to short circuit above,
+    # because then there's two exit paths to this code!)
+    with location.open("rb") as keyFile:
+        privateKey = serialization.load_pem_private_key(
+            keyFile.read(), password=None, backend=default_backend()
+        )
+        return Key(privateKey)
+
+
 class Key:
     """
     An object representing a key.  A key can be either a public or
@@ -1863,7 +1906,6 @@ class Key:
             return False
         else:
             return True
-
     def isSecurityKey(self):
         """
         Return True if key is an OpenSSH security key.
@@ -1887,46 +1929,3 @@ class Key:
         Modifies the application value for OpenSSH sk SSH key types.
         """
         self._application = value
-
-
-def _getPersistentRSAKey(location, keySize=4096):
-    """
-    This function returns a persistent L{Key}.
-
-    The key is loaded from a PEM file in C{location}. If it does not exist, a
-    key with the key size of C{keySize} is generated and saved.
-
-    @param location: Where the key is stored.
-    @type location: L{twisted.python.filepath.FilePath}
-
-    @param keySize: The size of the key, if it needs to be generated.
-    @type keySize: L{int}
-
-    @returns: A persistent key.
-    @rtype: L{Key}
-    """
-    location.parent().makedirs(ignoreExistingDirectory=True)
-
-    # If it doesn't exist, we want to generate a new key and save it
-    if not location.exists():
-        privateKey = rsa.generate_private_key(
-            public_exponent=65537, key_size=keySize, backend=default_backend()
-        )
-
-        pem = privateKey.private_bytes(
-            encoding=serialization.Encoding.PEM,
-            format=serialization.PrivateFormat.TraditionalOpenSSL,
-            encryption_algorithm=serialization.NoEncryption(),
-        )
-
-        location.setContent(pem)
-
-    # By this point (save any hilarious race conditions) we should have a
-    # working PEM file. Load it!
-    # (Future archaeological readers: I chose not to short circuit above,
-    # because then there's two exit paths to this code!)
-    with location.open("rb") as keyFile:
-        privateKey = serialization.load_pem_private_key(
-            keyFile.read(), password=None, backend=default_backend()
-        )
-        return Key(privateKey)
