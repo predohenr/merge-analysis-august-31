031f0e7b47c9cdfc3c031b5ff38e878b07022553
Mining Worker <miningworker@gmail.com>
2026-09-02 06:37:39 +0000

    Mining Framework Analysis for merge.mergiraf_semi_plus.go

--- pkg/vif/stack.go
+++ pkg/vif/stack.go
@@ -4,6 +4,28 @@ import (
 	"context"
 	"fmt"
 	"net"
+	"runtime"
+	"time"
+
+	"gvisor.dev/gvisor/pkg/tcpip"
+	"gvisor.dev/gvisor/pkg/tcpip/adapters/gonet"
+	"gvisor.dev/gvisor/pkg/tcpip/header"
+	"gvisor.dev/gvisor/pkg/tcpip/network/ipv4"
+	"gvisor.dev/gvisor/pkg/tcpip/network/ipv6"
+	"gvisor.dev/gvisor/pkg/tcpip/stack"
+	"gvisor.dev/gvisor/pkg/tcpip/transport/icmp"
+	"gvisor.dev/gvisor/pkg/tcpip/transport/tcp"
+	"gvisor.dev/gvisor/pkg/tcpip/transport/udp"
+	"gvisor.dev/gvisor/pkg/waiter"
+
+	"github.com/datawire/dlib/dlog"
+	"github.com/telepresenceio/telepresence/v2/pkg/iputil"
+	"github.com/telepresenceio/telepresence/v2/pkg/tunnel"
+)
+import (
+	"context"
+	"fmt"
+	"net"
 	"net/netip"
 	"time"
 
@@ -23,7 +45,7 @@ import (
 	"github.com/telepresenceio/telepresence/v2/pkg/tunnel"
 )
 
-func NewStack(ctx context.Context, dev stack.LinkEndpoint, streamCreator tunnel.StreamCreator) (*stack.Stack, error) {
+func NewStack(ctx context.Context, ep stack.LinkEndpoint, streamCreator tunnel.StreamCreator) (*stack.Stack, error) {
 	s := stack.New(stack.Options{
 		NetworkProtocols: []stack.NetworkProtocolFactory{
 			ipv4.NewProtocol,
@@ -40,7 +62,8 @@ func NewStack(ctx context.Context, dev stack.LinkEndpoint, streamCreator tunnel.
 	if err := setDefaultOptions(s); err != nil {
 		return nil, err
 	}
-	if err := setNIC(ctx, s, dev); err != nil {
+	_, err := setNIC(s, ep)
+	if err != nil {
 		return nil, err
 	}
 	setTCPHandler(ctx, s, streamCreator)
@@ -48,35 +71,34 @@ func NewStack(ctx context.Context, dev stack.LinkEndpoint, streamCreator tunnel.
 	return s, nil
 }
 
-const (
-	myWindowScale    = 6
-	maxReceiveWindow = 1 << (myWindowScale + 14) // 1MiB
-)
-
-// maxInFlight specifies the max number of in-flight connection attempts.
-const maxInFlight = 512
-
-// keepAliveIdle is used as the very first alive interval. Subsequent intervals
-// use keepAliveInterval.
-const keepAliveIdle = 60 * time.Second
-
-// keepAliveInterval is the interval between sending keep-alive packets.
-const keepAliveInterval = 30 * time.Second
-
-// keepAliveCount is the max number of keep-alive probes that can be sent
-// before the connection is killed due to lack of response.
-const keepAliveCount = 9
-
-type idStringer stack.TransportEndpointID
-
-func (i idStringer) String() string {
-	return fmt.Sprintf("%s -> %s",
-		iputil.JoinIpPort(i.RemoteAddress.AsSlice(), i.RemotePort),
-		iputil.JoinIpPort(i.LocalAddress.AsSlice(), i.LocalPort))
-}
-
 func setDefaultOptions(s *stack.Stack) error {
-	// Forwarding
+	sa := tcpip.TCPSACKEnabled(true)
+	if err := s.SetTransportProtocolOption(tcp.ProtocolNumber, &sa); err != nil {
+		return fmt.Errorf("SetTransportProtocolOption(tcp, TCPSACKEnabled(%t): %s", sa, err)
+	}
+
+	if runtime.GOOS == "windows" {
+		// Windows w/RACK performs poorly. ACKs do not appear to be handled in a
+		// timely manner, leading to spurious retransmissions and a reduced
+		// congestion window.
+		tr := tcpip.TCPRecovery(0)
+		if err := s.SetTransportProtocolOption(tcp.ProtocolNumber, &tr); err != nil {
+			return fmt.Errorf("SetTransportProtocolOption(tcp, TCPRecovery(%d): %s", tr, err)
+		}
+	}
+
+	// Enable Receive Buffer Auto-Tuning, see:
+	// https://github.com/google/gvisor/issues/1666
+	mo := tcpip.TCPModerateReceiveBufferOption(true)
+	if err := s.SetTransportProtocolOption(tcp.ProtocolNumber, &mo); err != nil {
+		return fmt.Errorf("SetTransportProtocolOption(tcp, TCPModerateReceiveBufferOption(%t): %s", mo, err)
+	}
+
+	cco := tcpip.CongestionControlOption("cubic")
+	if err := s.SetTransportProtocolOption(tcp.ProtocolNumber, &cco); err != nil {
+		return fmt.Errorf("SetTransportProtocolOption(tcp, CongestionControlOption(%s): %s", cco, err)
+	}
+
 	if err := s.SetForwardingDefaultAndAllNICs(ipv4.ProtocolNumber, true); err != nil {
 		return fmt.Errorf("SetForwardingDefaultAndAllNICs(ipv4, %t): %s", true, err)
 	}
@@ -93,28 +115,24 @@ func setDefaultOptions(s *stack.Stack) error {
 	return nil
 }
 
-func setNIC(ctx context.Context, s *stack.Stack, ep stack.LinkEndpoint) error {
+func setNIC(s *stack.Stack, ep stack.LinkEndpoint) (tcpip.NICID, error) {
 	nicID := s.NextNICID()
-	if err := s.CreateNICWithOptions(nicID, ep, stack.NICOptions{Name: "tel", Context: ctx}); err != nil {
-		return fmt.Errorf("create NIC failed: %s", err)
+	if tcpErr := s.CreateNIC(nicID, ep); tcpErr != nil {
+		return 0, fmt.Errorf("create NIC failed: %s", tcpErr)
 	}
 	if err := s.SetPromiscuousMode(nicID, true); err != nil {
-		return fmt.Errorf("SetPromiscuousMode(%d, %t): %s", nicID, true, err)
+		return 0, fmt.Errorf("SetPromiscuousMode(%d, %t): %s", nicID, true, err)
 	}
 	if err := s.SetSpoofing(nicID, true); err != nil {
-		return fmt.Errorf("SetSpoofing(%d, %t): %s", nicID, true, err)
+		return 0, fmt.Errorf("SetSpoofing(%d, %t): %s", nicID, true, err)
 	}
 	s.SetRouteTable([]tcpip.Route{
 		{
 			Destination: header.IPv4EmptySubnet,
 			NIC:         nicID,
 		},
-		{
-			Destination: header.IPv6EmptySubnet,
-			NIC:         nicID,
-		},
 	})
-	return nil
+	return nicID, nil
 }
 
 func forwardTCP(ctx context.Context, streamCreator tunnel.StreamCreator, fr *tcp.ForwarderRequest) {
@@ -128,69 +146,41 @@ func forwardTCP(ctx context.Context, streamCreator tunnel.StreamCreator, fr *tcp
 		}
 	}()
 
-	wq := waiter.Queue{}
+	var wq waiter.Queue
 	if ep, err = fr.CreateEndpoint(&wq); err != nil {
 		fr.Complete(true)
+		dlog.Error(ctx, err)
 		return
 	}
-	defer fr.Complete(false)
+	fr.Complete(false)
 
 	so := ep.SocketOptions()
 	so.SetKeepAlive(true)
 
 	idle := tcpip.KeepaliveIdleOption(keepAliveIdle)
 	if err = ep.SetSockOpt(&idle); err != nil {
+		dlog.Error(ctx, err)
 		return
 	}
 
 	ivl := tcpip.KeepaliveIntervalOption(keepAliveInterval)
 	if err = ep.SetSockOpt(&ivl); err != nil {
+		dlog.Error(ctx, err)
 		return
 	}
 
 	if err = ep.SetSockOptInt(tcpip.KeepaliveCountOption, keepAliveCount); err != nil {
+		dlog.Error(ctx, err)
 		return
 	}
 	dispatchToStream(ctx, newConnID(header.TCPProtocolNumber, id), gonet.NewTCPConn(&wq, ep), streamCreator)
 }
 
 func setTCPHandler(ctx context.Context, s *stack.Stack, streamCreator tunnel.StreamCreator) {
-	if err := s.SetTransportProtocolOption(tcp.ProtocolNumber,
-		&tcpip.TCPSendBufferSizeRangeOption{
-			Min:     tcp.MinBufferSize,
-			Default: tcp.DefaultSendBufferSize,
-			Max:     tcp.MaxBufferSize,
-		}); err != nil {
-		return
-	}
-
-	if err := s.SetTransportProtocolOption(tcp.ProtocolNumber,
-		&tcpip.TCPReceiveBufferSizeRangeOption{
-			Min:     tcp.MinBufferSize,
-			Default: tcp.DefaultSendBufferSize,
-			Max:     tcp.MaxBufferSize,
-		}); err != nil {
-		return
-	}
-
-	sa := tcpip.TCPSACKEnabled(true)
-	s.SetTransportProtocolOption(tcp.ProtocolNumber, &sa)
-
-	// Enable Receive Buffer Auto-Tuning, see:
-	// https://github.com/google/gvisor/issues/1666
-	mo := tcpip.TCPModerateReceiveBufferOption(true)
-	s.SetTransportProtocolOption(tcp.ProtocolNumber, &mo)
-
 	f := tcp.NewForwarder(s, maxReceiveWindow, maxInFlight, func(fr *tcp.ForwarderRequest) {
 		forwardTCP(ctx, streamCreator, fr)
 	})
 	s.SetTransportProtocolHandler(tcp.ProtocolNumber, f.HandlePacket)
-}
-
-var blockedUDPPorts = map[uint16]bool{ //nolint:gochecknoglobals // constant
-	137: true, // NETBIOS Name Service
-	138: true, // NETBIOS Datagram Service
-	139: true, // NETBIOS
 }
 
 func forwardUDP(ctx context.Context, streamCreator tunnel.StreamCreator, fr *udp.ForwarderRequest) {
@@ -237,4 +227,37 @@ func dispatchToStream(ctx context.Context, id tunnel.ConnID, conn net.Conn, stre
 	}
 	ep := tunnel.NewConnEndpoint(stream, conn, cancel, nil, nil)
 	ep.Start(ctx)
+}
+
+const (
+	myWindowScale    = 6
+	maxReceiveWindow = 4 << (myWindowScale + 14) // 4MiB
+)
+
+// maxInFlight specifies the max number of in-flight connection attempts.
+const maxInFlight = 16
+
+// keepAliveIdle is used as the very first alive interval. Subsequent intervals
+// use keepAliveInterval.
+const keepAliveIdle = 60 * time.Second
+
+// keepAliveInterval is the interval between sending keep-alive packets.
+const keepAliveInterval = 30 * time.Second
+
+// keepAliveCount is the max number of keep-alive probes that can be sent
+// before the connection is killed due to lack of response.
+const keepAliveCount = 9
+
+type idStringer stack.TransportEndpointID
+
+func (i idStringer) String() string {
+	return fmt.Sprintf("%s -> %s",
+		iputil.JoinIpPort(i.RemoteAddress.AsSlice(), i.RemotePort),
+		iputil.JoinIpPort(i.LocalAddress.AsSlice(), i.LocalPort))
+}
+
+var blockedUDPPorts = map[uint16]bool{ //nolint:gochecknoglobals // constant
+	137: true, // NETBIOS Name Service
+	138: true, // NETBIOS Datagram Service
+	139: true, // NETBIOS
 }
