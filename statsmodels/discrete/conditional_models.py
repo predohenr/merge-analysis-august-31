c4980169272e8b1fe39b61222af36ca3b21a4b13
Mining Worker <miningworker@gmail.com>
2026-09-02 16:35:02 +0000

    Mining Framework Analysis for merge.mergiraf_semi_plus.py

--- statsmodels/discrete/conditional_models.py
+++ statsmodels/discrete/conditional_models.py
@@ -1,21 +1,23 @@
 """
 Conditional logistic, Poisson, and multinomial logit regression
 """
-
+import statsmodels.base.model as base
 import collections
 import itertools
 import warnings
-
+import statsmodels.regression.linear_model as lm
+from statsmodels.formula.formulatools import advance_eval_env
 import numpy as np
-
-import statsmodels.base.model as base
 import statsmodels.base.wrapper as wrap
 from statsmodels.discrete.discrete_model import (
     MultinomialResults,
     MultinomialResultsWrapper,
 )
-from statsmodels.formula.formulatools import advance_eval_env
+import warnings
+import itertools
+import collections
 import statsmodels.regression.linear_model as lm
+from scipy.special import logsumexp
 
 
 class _ConditionalModel(base.LikelihoodModel):
@@ -191,8 +193,6 @@ class _ConditionalModel(base.LikelihoodModel):
                               start_params=start_params,
                               refit=refit,
                               **defaults)
-
-    # Override to allow groups to be passed as a variable name.
     @classmethod
     def from_formula(cls,
                      formula,
@@ -218,6 +218,8 @@ class _ConditionalModel(base.LikelihoodModel):
             formula, data=data, groups=groups, *args, **kwargs)
 
         return model
+
+    # Override to allow groups to be passed as a variable name.
 
 
 class ConditionalLogit(_ConditionalModel):
