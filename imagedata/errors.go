77f56ad7823dec1890d2939a69b3a20c0d55bf83
Mining Worker <miningworker@gmail.com>
2026-09-02 04:18:18 +0000

    Mining Framework Analysis for merge.mergiraf_semi_plus.go

--- imagedata/errors.go
+++ imagedata/errors.go
@@ -20,11 +20,36 @@ func newFileSizeError() error {
 	)
 }
 
-func (e FileSizeError) Error() string { return "Source image file is too big" }
-
 func wrapDownloadError(err error, desc string) error {
 	return ierrors.Wrap(
 		fetcher.WrapError(err), 0,
 		ierrors.WithPrefix(fmt.Sprintf("can't download %s", desc)),
 	)
 }
+
+func newImageResponseStatusError(status int, body string) error {
+	var msg string
+
+	if len(body) > 0 {
+		msg = fmt.Sprintf("Status: %d; %s", status, body)
+	} else {
+		msg = fmt.Sprintf("Status: %d", status)
+	}
+
+	statusCode := 404
+	if status >= 400 && status < 500 {
+		statusCode = status
+	} else if status >= 500 {
+		statusCode = http.StatusBadGateway
+	}
+
+	return ierrors.Wrap(
+		ImageResponseStatusError(msg),
+		1,
+		ierrors.WithStatusCode(statusCode),
+		ierrors.WithPublicMessage(msgSourceImageIsUnreachable),
+		ierrors.WithShouldReport(false),
+	)
+}
+
+func (e FileSizeError) Error() string { return "Source image file is too big" }
