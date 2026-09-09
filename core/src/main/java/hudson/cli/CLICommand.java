9c3c01c4fe99a5d00b6f1c2a5227bae303d898f4
Mining Worker <miningworker@gmail.com>
2026-09-09 03:26:29 +0000

    Mining Framework Analysis for merge.s3m.java

--- core/src/main/java/hudson/cli/CLICommand.java
+++ core/src/main/java/hudson/cli/CLICommand.java
@@ -125,7 +125,7 @@ public abstract class CLICommand implements ExtensionPoint, Cloneable {
      * the message to the server log file, which is probably not what you want.
      */
     @SuppressFBWarnings(value = "PA_PUBLIC_PRIMITIVE_ATTRIBUTE", justification = "Preserve API compatibility")
-    public transient PrintStream stdout, stderr;
+public transient PrintStream stdout, stderr;
 
     /**
      * Shared text, which is reported back to CLI if an error happens in commands
@@ -141,7 +141,7 @@ public abstract class CLICommand implements ExtensionPoint, Cloneable {
      * This input stream is buffered to hide the latency in the remoting.
      */
     @SuppressFBWarnings(value = "PA_PUBLIC_PRIMITIVE_ATTRIBUTE", justification = "Preserve API compatibility")
-    public transient InputStream stdin;
+public transient InputStream stdin;
 
     /**
      * @deprecated No longer used.
@@ -153,7 +153,7 @@ public abstract class CLICommand implements ExtensionPoint, Cloneable {
      * The locale of the client. Messages should be formatted with this resource.
      */
     @SuppressFBWarnings(value = "PA_PUBLIC_PRIMITIVE_ATTRIBUTE", justification = "Preserve API compatibility")
-    public transient Locale locale;
+public transient Locale locale;
 
     /**
      * The encoding of the client, if defined.
